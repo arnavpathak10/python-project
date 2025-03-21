7017a47adbc664ec37c7c90f15abd23b926bf377
arnavpathak10 <arnavpathak.in@gmail.com>
2025-03-21 22:13:29 +0530

    visitor_records_book.py

--- visitor_records_book.py
+++ visitor_records_book.py
@@ -4,36 +4,35 @@ import datetime
 try:
     conn = mysql.connector.connect(user = "root", password = "root", host = "localhost", port = 3306)
     if(conn.is_connected()):
-        print(True)
+        print("Connected Successfully to mysql server")
 except:
-    print("unable to make connection")
+    print("Unable to make connection")
 
 sql = "CREATE DATABASE visitor_record"
 try:
     myc = conn.cursor()
     myc.execute(sql)
+    print("Database created")
 except:
-    print("unable to create database")
-myc.close()
-conn.close()"""
+    print("Unable to create database")
+
 
 #creating table 
-config = {"user":"root", "password":"root", "database":"visitor_record", "host":"localhost", "port":3306}
+config = {"user" : "root", "password" : "root", "database" : "visitor_record", "host" : "localhost", "port" : "3306"}
 try:
     connection = mysql.connector.connect(**config)
-    if(connection.is_connected()):
-        print("Connection Established")
+    if (connection.is_connected):
+        print("Successfully connected to database")
 except:
-    print("unable to connect")
-"""sql1 = "CREATE TABLE visitor(Sr_no INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(25), Address VARCHAR(50), Date varchar(20), Time VARCHAR(20), Contact INT)"
+    print("Unable to connect with database")
+sql1 = "CREATE TABLE visitor(Sr_no INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(25), Address VARCHAR(50), Date varchar(20), Time VARCHAR(20), Contact INT)"
 try:
-    mycur = connection.cursor()
-    mycur.execute(sql1)
+    myc_table = connection.cursor()
+    myc_table.execute(sql1)
     print("Table created")
 except:
     print("Unable to create table")
-mycur.close()
-connection.close()
+
 
 #enter visitor records
 def insert_record(n, a, d, t, c):
@@ -52,12 +51,13 @@ while True:
     inp = input("\nDo you want to enter records (yes/no) : ")
     if(inp.lower()=="yes" or inp.lower()=="y"):
         name = input("Enter your name : ").title()
-        add = input("Enter your add : ").title()
+        add = input("Enter your address : ").title()
         date = datetime.datetime.now().date()
         time = datetime.datetime.now().time()
         contact = int(input("Enter Mobile number : "))
         insert_record(name, add, date, time, contact)
-    sleep(6)
+    
+    sleep(4)
     inp = input("\nDo you want to display all records (yes/no) : ")
     if(inp.lower()=="yes" or inp.lower()=="y"):
         query = "SELECT * FROM visitor"
@@ -66,6 +66,5 @@ while True:
         rows = mycur_obj.fetchall()
         for row in rows:
             print(f"sr.no : {row[0]:5d}  Name : {row[1]:15s}  Address : {row[2]:15}  Date : {row[3]:10}  Time : {row[4]:10}  Contact : {row[5]:10}")
-
 
    
